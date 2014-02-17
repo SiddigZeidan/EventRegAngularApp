ed15baf58f303534ffa9cf651f6058aef71ad95c
Siddig Zeidan <siddigie1234@gmail.com>
2014-02-17 11:51:18 +0000

    initial commit

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,42 @@
+'use strict';
+
+eventsApp.controller('EventController',
+	function EventController($scope) {
+		$scope.snippet = "<span style = 'color:red'> Hi There</span>";
+		$scope.event = {
+			name: 'Angular Boot Camp',
+			date: '1/1/2013',
+			time: '10:30 am',
+			location: {
+					address: 'Google HQ',
+					city: 'Mountain View',
+					provience: 'CA'
+			},
+			sessions: [
+
+					{
+						name: 'Directives Masterclass',
+						creatorName: 'Bob Smith',
+						upVoteCount: 0
+					},
+					{
+						name: 'Scopes for fun and profit',
+						creatorName: 'Bob Smith',
+						upVoteCount: 0
+					},
+					{
+						name: 'Well Behaved Controllers',
+						creatorName: 'Bob Smith',
+						upVoteCount: 0
+					}
+
+			]
+		}
+		$scope.upVoteSession = function(session){
+			session.upVoteCount++;
+		}
+			$scope.downVoteSession = function(session){
+			session.upVoteCount--;
+		}
+	}
+	);
